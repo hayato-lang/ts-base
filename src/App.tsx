28172151cc07ac82b9d332242dea31fb52000a2a
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-23 18:15:56 +0900

    取得するデータに型を定義

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
+import { Todo } from './Todo';
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/users").then((res) =>{
-      console.log(res.data);
+    axios.get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos").then((res) =>{
+      setTodos(res.data);
     })
   }
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo key={todo.userId} title={todo.title} userId={todo.userId}/>
+      ))}
     </div>
   );
 }
